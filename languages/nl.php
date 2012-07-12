cac76c8d1c641c24f5e5310cbb5facc9f90792b5
jdalsem <jdalsem@coldtrick.com>
2012-07-12 10:22:54 +0200

    fixed some incorrect translations

--- languages/nl.php
+++ languages/nl.php
@@ -5,7 +5,7 @@
 		
 		// Admin configuration
 		'login_redirector:admin:first_login:title' => "Eerste aanmelding doorverwijs instellingen",	
-		'login_redirector:admin:title' => "Algemene doorverwijs installeingen",
+		'login_redirector:admin:title' => "Algemene doorverwijs instellingen",
 	
 		'login_redirector:admin:first_login:config' => "Selecteer de pagina waar gebruikers naar worden doorverwezen bij de eerste aanmelding:",
 		'login_redirector:admin:first_login:custom_redirect' => "Aangepaste doorverwijziging:",
@@ -23,7 +23,7 @@
 		'login_redirector:admin:custom_redirect_info:title' => "Aangepaste URL opties",
 		'login_redirector:admin:custom_redirect_info' => "Om een gebruiker door te verwijzen naar een pagina naar keuze, voer de URL in.<br />Voer de complete url in (inclusief http of https) of gebruik een van de volgende (inclusief de blok haken):",
 	
-		'login_redirector:admin:custom_redirect_list' => "Voeg extra pagina's to waar gebruikers uit kunnen kiezen (komma gescheiden in de vorm: titel|url)",
+		'login_redirector:admin:custom_redirect_list' => "Voeg extra pagina's toe waar gebruikers uit kunnen kiezen (komma gescheiden in de vorm: titel|url)",
 		
 		'login_redirector:user:config' => "Selecteer op welke pagina je terecht komt als je je aanmeld:",
 		
